57c480825c4a015585ec5503934e4ae106f98d7d
Tim Willebrands <timwillebrands@gmail.com>
2026-03-28 12:56:59 +0100

    Integrated logging viewer

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,6 +5,9 @@ using Orleans.EventSourcing;
 using PartyTown.Configuration;
 using PartyTown.Logging;
 using PartyTown.Services.Realtime;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -64,12 +67,43 @@ builder.Host.UseOrleans(siloBuilder =>
     siloBuilder.AddActivityPropagation();
 });
 
-// Configure console logging with scopes
+// Configure console logging with scopes and OpenTelemetry
 builder.Logging.AddSimpleConsole(options =>
 {
     options.IncludeScopes = true;
     options.TimestampFormat = "HH:mm:ss ";
+})
+.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
 });
+
+var otel = builder.Services.AddOpenTelemetry();
+
+// Add Metrics for ASP.NET Core and our custom metrics and export via OTLP
+otel.WithMetrics(metrics =>
+{
+    // Metrics provider from OpenTelemetry
+    metrics.AddAspNetCoreInstrumentation();
+    // Metrics provides by ASP.NET Core in .NET 8
+    metrics.AddMeter("Microsoft.AspNetCore.Hosting");
+    metrics.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+});
+
+// Add Tracing for ASP.NET Core and our custom ActivitySource and export via OTLP
+otel.WithTracing(tracing =>
+{
+    tracing.AddAspNetCoreInstrumentation();
+    tracing.AddHttpClientInstrumentation();
+});
+
+// Export OpenTelemetry data via OTLP, using env vars for the configuration
+var OtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+if (OtlpEndpoint != null)
+{
+    otel.UseOtlpExporter();
+}
 
 var app = builder.Build();
 
